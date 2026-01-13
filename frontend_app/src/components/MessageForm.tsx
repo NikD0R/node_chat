7e1e14d70a0d6dd5aaaf1cc90192f09828d24a32
Nikita Doroshenko <nikitokd05@gmail.com>
2026-01-13 22:45:36 +0200

    my solution

--- frontend_app/src/components/MessageForm.tsx
+++ frontend_app/src/components/MessageForm.tsx
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import { useRooms } from "../context/RoomsContext";
 import { useAuth } from "../context/AuthContext";
 import api from "../api/axios";
-import { useMessages } from "../context/MessagesContext";
 
 export const MessageForm: React.FC = () => {
   const [text, setText] = useState("");
   const { username } = useAuth();
   const { currentRoomId } = useRooms();
-  const { addLocalMessage } = useMessages(); // optional local echo
 
   const send = async () => {
     if (!currentRoomId || !username || !text.trim()) return;
